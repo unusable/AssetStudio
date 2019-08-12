b7ec256b943199a162aa203fe75716d985474d9b
unusable <unusable@163.com>
2019-08-12 11:24:37 +0800

    add new fbx project

---
+++ AssetStudioFBX_2/FbxExporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace AssetStudio
+{ 
+    public static class Fbx
+    {
+        public static Vector3 QuaternionToEuler(Quaternion q)
+        {
+            return Vector3.Zero;
+        }
+
+        public static class Exporter
+        {
+            public static void Export(string path, IImported imported, bool eulerFilter, float filterPrecision, bool allNodes, bool skins, bool animation, bool blendShape, bool castToBone, float boneSize, float scaleFactor, int versionIndex, bool isAscii)
+            {
+
+            }
+        }
+    }
+}
